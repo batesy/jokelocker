7008f83d2378ed8900794db1553a26fafa772bcf
batesy <gbatesy@gmail.com>
2013-12-03 12:03:08 -0500

    Generate Users controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202025254) do
+ActiveRecord::Schema.define(version: 20131203165335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20131202025254) do
     t.string   "title"
     t.string   "punchline"
     t.string   "format"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username",         null: false
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
