3231828a9baa7abda5a9f057fab7fbd2034a06bf
batesy <gbatesy@gmail.com>
2013-12-04 00:55:07 -0500

    Added a welcome page prompting user to Login or Signup.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      redirect_to :jokes
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Jokelocker3::Application.routes.draw do
-  root to: 'jokes#index'
+  root to: 'welcome#index'
 
   resources :jokes
   resources :user_sessions
   resources :users
 
+  get 'signup' => 'users#new', :as => :signup
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
 end
